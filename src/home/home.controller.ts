b9d987bf791dfc4c373ae007e6905d47eef268b9
Ardhika Ryzha Nurmawan <ardhikarn@gmail.com>
2023-08-27 16:24:09 +0700

    implement guard

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -9,12 +9,17 @@ import {
   Put,
   Query,
   UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { CreateHomeDto, HomeResponseDto, UpdateHomeDto } from './dto/home.dto';
-import { ProperType } from '@prisma/client';
+import { ProperType, UserType } from '@prisma/client';
 import { User } from 'src/user/decoratos/user.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { MyControllerMetadata } from 'src/decorators/contoh.decorator';
 
+@MyControllerMetadata('contoh ambil metadata dari getClass()')
 @Controller('home')
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
@@ -48,11 +53,15 @@ export class HomeController {
     return this.homeService.getHomeById(id);
   }
 
+  @Roles(UserType.REALTOR)
+  @UseGuards(AuthGuard)
   @Post()
   createHome(@Body() body: CreateHomeDto, @User() user: User): Promise<HomeResponseDto> {
     return this.homeService.createHome(body, user.id);
   }
 
+  @Roles(UserType.REALTOR)
+  @UseGuards(AuthGuard)
   @Put(':id')
   async updateHome(
     @Param('id', ParseIntPipe) id: number,
@@ -66,6 +75,8 @@ export class HomeController {
     return this.homeService.updateHome(id, body);
   }
 
+  @Roles(UserType.REALTOR)
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async deleteHome(
     @Param('id', ParseIntPipe) id: number,
--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Post, Body, Param, UnauthorizedException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  UnauthorizedException,
+  Get,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GenerateProductKeyDto, SigninDto, SignupDto } from '../dtos/auth.dto';
 import { UserType } from '@prisma/client';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
+import { User } from '../decoratos/user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -10,17 +18,20 @@ export class AuthController {
 
   @Post('/signup/:userType')
   async signup(@Body() body: SignupDto, @Param('userType') userType: UserType) {
-    const userTypeUpper = userType.toUpperCase() as UserType
-    if(userTypeUpper !== UserType.BUYER) {
-      if(!body.productKey) {
-        throw new UnauthorizedException()
+    const userTypeUpper = userType.toUpperCase() as UserType;
+    if (userTypeUpper !== UserType.BUYER) {
+      if (!body.productKey) {
+        throw new UnauthorizedException();
       }
 
       const validProductKey = `${body.email}-${userType}-${process.env.PRODUCT_SECRET_KEY}`;
-      const isValidProductKey = await bcrypt.compare(validProductKey, body.productKey )
+      const isValidProductKey = await bcrypt.compare(
+        validProductKey,
+        body.productKey,
+      );
 
-      if(!isValidProductKey) {
-        throw new UnauthorizedException()
+      if (!isValidProductKey) {
+        throw new UnauthorizedException();
       }
     }
     return this.authService.signup(body, userTypeUpper);
@@ -34,5 +45,15 @@ export class AuthController {
   @Post('/key')
   generateProductKey(@Body() { email, userType }: GenerateProductKeyDto) {
     return this.authService.generateProductKey(email, userType);
+  }
+
+  @Get('/me')
+  me(@User() user: User) {
+    return user;
+  }
+
+  @Post('/logout')
+  logout() {
+    return this.authService.logout();
   }
 }
